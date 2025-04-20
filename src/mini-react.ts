37388e8f99f533b9fff5289932769051fa04a95b
tomoki33kurita <tomoki33kurita@gmail.com>
2025-04-20 14:10:34 +0900

    reconcileChildren関数を実装

--- src/mini-react.ts
+++ src/mini-react.ts
@@ -84,8 +84,9 @@ const Fragment = Symbol.for("react.fragment")
 })(window)
 
 // 引数がVirtualElementかどうかを判定する関数
-const isVirtualELement = (e: unknown): e is VirtualElement =>
-  typeof e === "object"
+const isVirtualElement = (e: unknown): e is VirtualElement => {
+  return typeof e === "object"
+}
 
 // type:"TEXT"の要素となるobjectを作成する関数
 const createTextElement = (text: string): VirtualElement => ({
@@ -104,7 +105,7 @@ const createElement = (
   ...child: (unknown | VirtualElement)[]
 ): VirtualElement => {
   const children = child.map((c) => {
-    isVirtualELement(c) ? c : createTextElement(String(c))
+    return isVirtualElement(c) ? c : createTextElement(String(c))
   })
 
   return {
@@ -175,6 +176,179 @@ const createDOM = (fiberNode) => {
   return DOM
 }
 
+const isDef = <T>(param: T): param is NonNullable<T> => {
+  return param !== void 0 && param !== null
+}
+
+const isPlainObject = (val: unknown): val is Record<string, unknown> => {
+  return (
+    Object.prototype.toString.call(val) === "[object Object]" &&
+    [Object.prototype, null].includes(Object.getPrototypeOf(val))
+  )
+}
+
+// Change the DOM based on fiber node changes.
+// Note that we must complete the comparison of all fiber nodes before commitRoot.
+// The comparison of fiber nodes can be interrupted, but the commitRoot cannot be interrupted.
+// Reactのコミットフェーズの役割
+const commitRoot = () => {
+  // 親fiberを見つける、定義づける関数
+  const findParentFiber = (fiberNode?: FiberNode) => {
+    if (fiberNode) {
+      let parentFiber = fiberNode.return
+      while (parentFiber && !parentFiber.dom) {
+        parentFiber = parentFiber.return
+      }
+      return parentFiber
+    }
+    return null
+  }
+
+  // DOMを消す関数
+  const commitDeletion = (
+    parentDOM: FiberNodeDom,
+    DOM: NonNullable<FiberNodeDom>
+  ) => {
+    if (isDef(parentDOM)) {
+      // parentDOMが存在する場合は、DOMを削除する
+      // つまり親要素にDOMが存在する場合は、DOMを削除する
+      parentDOM.removeChild(DOM)
+    }
+  }
+
+  // DOMを置き換える関数
+  const commitReplacement = (
+    parentDOM: FiberNodeDom,
+    DOM: NonNullable<FiberNodeDom>
+  ) => {
+    if (isDef(parentDOM)) {
+      // parentDOMが存在する場合は、DOMを置き換える
+      parentDOM.appendChild(DOM)
+    }
+  }
+  const commitWork = (fiberNode?: FiberNode) => {
+    if (fiberNode) {
+      if (fiberNode.dom) {
+        const parentFiber = findParentFiber(fiberNode)
+        const parentDOM = parentFiber?.dom
+
+        switch (fiberNode.effectTag) {
+          case "REPLACEMENT":
+            commitReplacement(parentDOM, fiberNode.dom)
+            break
+          case "UPDATE":
+            updateDOM(
+              fiberNode.dom,
+              fiberNode.alternate ? fiberNode.alternate.props : {},
+              fiberNode.props
+            )
+            break
+          default:
+            break
+        }
+      }
+
+      commitWork(fiberNode.child) // 再帰的に子要素を処理していく
+      commitWork(fiberNode.sibling) // 同じレベルの兄弟要素を再起的に処理していく
+    }
+  }
+
+  for (const deletion of deletions) {
+    if (deletion.dom) {
+      const parentFiber = findParentFiber(deletion)
+      commitDeletion(parentFiber?.dom, deletion.dom)
+    }
+  }
+
+  if (wipRoot !== null) {
+    // work-in-progress root
+    commitWork(wipRoot.child) // ルートの子要素のfiberNodeをcommitWorkで処理していく
+    currentRoot = wipRoot // 最新状態を反映
+  }
+}
+
+// Reactが規定している Reconciliationを模倣した関数
+const reconcileChildren = (
+  fiberNode: FiberNode, // 現在のFiberツリー
+  elements: VirtualElement[] = [] // 仮想DOMのリスト
+) => {
+  let index = 0 // ...現在処理中の要素(fiber)のインデックス
+  let oldFiberNode: FiberNode | undefined = void 0 // ... 前回レンダリング時のfiberツリーから取得した子ノード
+  let prevSibling: FiberNode | undefined = void 0 //  ... 直前に処理した兄弟ノード
+  const virtualElements = elements.flat(Infinity) // 1次元配列にする
+
+  // alternate === 前回レンダリング時のfiberNode
+  // 前回レンダリング時のfiberツリーから取得した子ノードをoldFiberNodeに格納
+  if (fiberNode.alternate?.child) {
+    oldFiberNode = fiberNode.alternate.child
+  }
+
+  // ここで新しいfiberNodeを作成する
+  while (
+    index < virtualElements.length || // ...仮想DOMの要素が残っている
+    typeof oldFiberNode !== "undefined" // ...前回レンダリング時のfiberツリーから取得した子ノードが残っている
+  ) {
+    const virtualElement = virtualElements[index]
+    let newFiber: FiberNode | undefined = void 0
+
+    const isSameType = Boolean(
+      oldFiberNode &&
+        virtualElement &&
+        oldFiberNode.type === virtualElement.type
+    )
+
+    if (isSameType && oldFiberNode) {
+      newFiber = {
+        type: oldFiberNode.type,
+        dom: oldFiberNode.dom,
+        alternate: oldFiberNode,
+        props: virtualElement.props, // propsを更新
+        return: fiberNode,
+        effectTag: "UPDATE", // 更新フラグを立てる
+      }
+    }
+    if (!isSameType && Boolean(virtualElement)) {
+      newFiber = {
+        type: virtualElement.type,
+        dom: null,
+        alternate: null,
+        props: virtualElement.props,
+        return: fiberNode,
+        effectTag: "REPLACEMENT",
+      }
+    }
+    if (!isSameType && oldFiberNode) {
+      // 古いfiberNodeが存在する場合は、fiberNodeを削除用の配列に格納
+      deletions.push(oldFiberNode)
+    }
+
+    if (oldFiberNode) {
+      //　古いfiberNodeが存在する場合は、その兄弟ノードをoldFiberNodeに格納して、次のループに進むよう仕向ける
+      // whileの条件式の1つに、"typeof oldFiberNode !== "undefined"があるので、古いfiberNodeが存在する限り、ループは続く
+      oldFiberNode = oldFiberNode.sibling
+    }
+
+    if (index === 0) {
+      fiberNode.child = newFiber
+    } else if (typeof prevSibling !== "undefined") {
+      // @ts-ignore
+      prevSibling.sibling = newFiber
+    }
+
+    prevSibling = newFiber
+    index++
+  }
+}
+
+const performUnitOfWork = (fiberNode: FiberNode): FiberNode | null => {
+  const { type } = fiberNode
+  switch (typeof type) {
+    case "function": {
+      wipFiber = fiberNode
+    }
+  }
+}
+
 const render = (element, container) => {
   // DOMを作る
   const DOM = createDOM(element)
@@ -191,3 +365,5 @@ const render = (element, container) => {
   // containerにDOMを追加する
   container.appendChild(DOM)
 }
+
+// useStateを実装する L414-
